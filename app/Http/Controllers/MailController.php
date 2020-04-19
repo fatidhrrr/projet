3d54df9c210ba7075ba1fbd66dd3adc89b4f5081
Dahri Fati <mohaadhr@MacBook-Air-de-Dahri.local>
2020-04-20 00:42:27 +0200

    fin

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,16 +5,25 @@ namespace App\Http\Controllers;
 use App\Mail\Contactmail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
+use App\Contact;
 
 class MailController extends Controller
 {
     public function store(Request $request)
     {
+        $contact = new Contact();
         $nom = $request->input('nom');
         $prenom = $request->input('prenom');
         $email = $request->input('email');
         $message = $request->input('message');
+        $password = $request->input('password');
+        $contact->nom = $nom;
+        $contact->prenom = $prenom;
+        $contact->email = $email;
+        $contact->message = $message;
+        $contact->password = $password;
         Mail::to('dhrfatima12@gmail.com')->send(new Contactmail($nom, $prenom, $email, $message));
+        $contact->save();
         return redirect()->back();
     }
 }
--- resources/views/contactmail.blade.php
+++ resources/views/contactmail.blade.php
@@ -1,6 +1,8 @@
-<h1>Vous avez reçu un message de:</h1>
+{{-- <h1>Vous avez reçu un message de:</h1>
 
 <p>{{$nom}}</p>
 <p>{{$prenom}}</p>
 <p>{{$email}}</p>
-<p>{{$msg}}</p>
+<p>{{$msg}}</p> --}}
+
+<h3>Merci pour votre message.</h3>
